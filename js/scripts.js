76eaf0e54550bff34a83ebda48e7c946c43f4ba1
Colt Thatcher <colt.thatcher303@gmail.com>
2021-10-18 14:33:08 -0700

    Add converstion functions to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+/*
+  Gallons to litres
+  Litres to gallons
+  Tablespoons to teaspoons 
+  teaspoons to tablespoons
+  tablespoons to cups
+  cups to pints
+  cups to quarts
+  pints to quarts
+  cups to fluid ounces
+  ounces to quarts
+  quarts to gallon
+*/
+
+function teaspoonsToTablespoons(number){
+  return number /= 3;
+  number = 
+}
+function tablespoonsToTeaspoons(number){
+  return number *= 3;
+}
+function tablespoonsToCups(number){
+  return number / 16;
+}
+function cupsToTablespoons (number){
+  return number * 16;
+}
+function cupsToPints (number){
+return number /= 2;
+}
+function cupsToQuarts (number){
+  return number /= 4;
+}
+function pintsToQuarts(number){
+  return number /= 2;
+}
+function cupsToFluidOunces(number){
+  return number *= 8;
+}
+function ouncesToQuarts(number){
+  return number 
+}
